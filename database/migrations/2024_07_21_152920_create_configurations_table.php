81d561186e80893ea96434b45f5c0db415d89353
Jason Advincula <jadvincula89@gmail.com>
2024-09-07 21:25:40 +0800

    no message

--- database/migrations/2024_07_21_152920_create_configurations_table.php
+++ database/migrations/2024_07_21_152920_create_configurations_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('configurations', function (Blueprint $table) {
-            $table->string('key', 155)->notNull();
-            $table->increments('id');
-            $table->string('value', 45)->nullable();
-            $table->string('bet_percentage', 45)->notNull();
-            $table->primary('id');
+            $table->id(); // Creates an auto-incrementing `id` column
+            $table->string('name', 155); // Creates a VARCHAR column for `name`
+            $table->string('value', 45)->nullable(); // Creates a VARCHAR column for `value` with nullable
+            $table->string('bet_percentage', 45); // Creates a VARCHAR column for `bet_percentage`
+
+            $table->primary('id'); // Sets `id` as the primary key
         });
     }
 
